2dcb8d8e80114129a1d8214f1b31d194b1f3ca0d
Vladimir <starsdogroba@yandex.ru>
2023-09-04 12:38:28 +0300

    refactoring

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -4,12 +4,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание тестового набора перелетов
         List<Flight> flights = FlightBuilder.createFlights();
 
-        // Создание экземпляра фильтра
         FlightFilter flightFilter = new FlightFilterImpl();
-
 
         List<Flight> filteredFlights1 = flightFilter.filterWhereDepartureBeforeCurrentTime(flights);
         System.out.println("\nПерелеты с вылетом не до текущего момента времени:");
